a4c19e38241a73f85417f8c1f6e38fa00e0fd48b
KseniiaKarpova <31700970+KseniiaKarpova@users.noreply.github.com>
2020-05-15 22:35:24 +0300

    Update task_40.py

--- task_40.py
+++ task_40.py
@@ -17,7 +17,7 @@ def floorRoot(n, s):
             return x
         x = y
     return 1
-'''
+
 
 msg=b'this is test'
 bitsize = 1024
@@ -70,4 +70,3 @@ str = str(floorRoot(result,3))
 print(binascii.unhexlify(bytes(str, encoding='utf-8')))
 
 
-'''
